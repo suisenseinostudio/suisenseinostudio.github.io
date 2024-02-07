ad009b7eb522a18e880545b900892c829f346a53
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-07 16:05:25 +0900

    Register sw.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+try{
+  navigator.serviceWorker.register("sw.js");
+}catch(err){
+  console.error(err);
+}
+
 document.querySelectorAll("[data-src]").forEach(elm=>{
   fetch(elm.dataset.src).then(res=>res.text()).then(txt=>{
     elm.innerHTML=txt;
