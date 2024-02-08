e3a49b310978eb27a48f25ebc01a0c30c976b8ba
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-08 18:55:42 +0900

    Rrreplace "window" with "self"

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener("install",e=>{
 const importKey=async(pass)=>{
   try{
     const data=(new TextEncoder()).encode(pass);
-    return window.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
+    return self.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
   }catch(err){
     console.error("error in importKey");
     console.error(err);
@@ -32,10 +32,10 @@ const importKey=async(pass)=>{
 
 const deriveKey=async(pass)=>{
   try{
-    const salt=window.crypto.getRandomValues(new Uint8Array(16));
+    const salt=self.crypto.getRandomValues(new Uint8Array(16));
     const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
     const base=await importKey(pass);
-    return window.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
+    return self.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
   }catch(err){
     console.error("error in deriveKey");
     console.error(err);
