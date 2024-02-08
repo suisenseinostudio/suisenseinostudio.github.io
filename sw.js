6227359c39987082171582cfff108b3b68c8b817
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-08 14:41:53 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,15 +7,27 @@ self.addEventListener("install",e=>{
   };
   req.onupgradeneeded=event=>{
     try{
-      db.createObjectStore("keys",{autoIncrement:true});
+      db.createObjectStore("pass");
     }catch(err){
       console.error(err);
     }
   };
 });
 
+const importKey=async(pass)=>{
+  const data=(new TextEncoder()).encode(pass);
+  return window.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
+};
+
+const deriveKey=async(pass)=>{
+  const salt=window.crypto.getRandomValues(new Uint8Array(16));
+  const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
+  const base=await importKey(pass);
+  return window.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
+};
+
 self.addEventListener("message",e=>{
-  db.transaction("keys","readwrite").objectStore("keys").add(e.data);
+  db.transaction("keys","readwrite").objectStore("pass").add(e.data,e.data);
 });
 
 const decrypt=async req=>{
@@ -23,10 +35,10 @@ const decrypt=async req=>{
   const iv=res.slice(0,8*12);
   const algo={name:"AES-GCM",iv};
   const data=res.slice(8*12,res.byteLength);
-  db.transaction("keys").openCursor().onsucess=async e=>{
+  db.transaction("pass").openCursor().onsucess=async e=>{
     const cursor=e.target.result;
     if(cursor){
-      const key=cursor.value;
+      const key=await deriveKey(cursor.value);
       const result=await crypto.subtle.decrypt(algo,key,data);
       if((new TextDecoder()).decode(result.slice(0,8*12))==(new TextDecoder()).decode(iv)){
         return new Blob([result.slice(8*12,result.byteLength)]);
@@ -45,3 +57,4 @@ self.addEventListener("fetch",e=>{
   }else{
     e.respondWith(fetch(e.request));
   }
+}
