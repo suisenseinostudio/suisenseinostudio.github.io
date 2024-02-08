8287222bdf6255b59faf047f9fca3e92149e7163
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-08 23:24:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ try{
   const rg=navigator.serviceWorker.register("sw.js");
   if(rg.active){
     rg.active.postMessage("hello");
-    document.querySelectorAll("[data-src]").forEach(elm=>{
-      fetch(elm.dataset.src).then(res=>res.text()).then(txt=>{
-        elm.innerHTML=txt;
-      });
-    });
   }
 }catch(err){
   console.error(err);
 }
+
+window.onload=()=>{
+  document.querySelectorAll("[data-src]").forEach(elm=>{
+    fetch(elm.dataset.src).then(res=>res.text()).then(txt=>{
+      elm.innerHTML=txt;
+    });
+  });
+};
