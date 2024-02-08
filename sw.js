48012979fce7032e2fca8db7046d90933b410aed
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-08 16:20:38 +0900

    Ddddebug sw.js

--- sw.js
+++ sw.js
@@ -15,15 +15,23 @@ self.addEventListener("install",e=>{
 });
 
 const importKey=async(pass)=>{
-  const data=(new TextEncoder()).encode(pass);
-  return window.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
+  try{
+    const data=(new TextEncoder()).encode(pass);
+    return window.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
+  }catch(err){
+    console.error("importKey:"+err);
+  }
 };
 
 const deriveKey=async(pass)=>{
-  const salt=window.crypto.getRandomValues(new Uint8Array(16));
-  const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
-  const base=await importKey(pass);
-  return window.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
+  try{
+    const salt=window.crypto.getRandomValues(new Uint8Array(16));
+    const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
+    const base=await importKey(pass);
+    return window.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
+  }catch(err){
+    console.error("deriveKey:"+err);
+  }
 };
 
 self.addEventListener("message",e=>{
@@ -36,17 +44,21 @@ const decrypt=async req=>{
   const algo={name:"AES-GCM",iv};
   const data=res.slice(8*12,res.byteLength);
   db.transaction("pass").openCursor().onsucess=async e=>{
-    const cursor=e.target.result;
-    if(cursor){
-      const key=await deriveKey(cursor.value);
-      const result=await crypto.subtle.decrypt(algo,key,data);
-      if((new TextDecoder()).decode(result.slice(0,8*12))==(new TextDecoder()).decode(iv)){
-        return new Blob([result.slice(8*12,result.byteLength)]);
+    try{
+      const cursor=e.target.result;
+      if(cursor){
+        const key=await deriveKey(cursor.value);
+        const result=await crypto.subtle.decrypt(algo,key,data);
+        if((new TextDecoder()).decode(result.slice(0,8*12))==(new TextDecoder()).decode(iv)){
+          return new Blob([result.slice(8*12,result.byteLength)]);
+        }else{
+          cursor.continue();
+        }
       }else{
-        cursor.continue();
+        return new Response(null,{status:401});
       }
-    }else{
-      return new Response(null,{status:401});
+    }catch(err){
+      console.error("importKey:"+err);
     }
   }
 };
