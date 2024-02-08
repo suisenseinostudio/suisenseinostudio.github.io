ff43a401441740f0548126ff87be4c40a779a5b3
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-08 13:41:15 +0900

    Ddefie decrypt()

--- sw.js
+++ sw.js
@@ -18,7 +18,24 @@ self.addEventListener("message",e=>{
   db.transaction("keys","readwrite").objectStore("keys").add(e.data);
 });
 
-const decrypt=req=>{
+const decrypt=async req=>{
+  const res=await (await fetch(req)).arrayBuffer();
+  const iv=res.slice(0,8*12);
+  const algo={name:"AES-GCM",iv};
+  const data=res.slice(8*12,res.byteLength);
+  db.transaction("keys").openCursor().onsucess=async e=>{
+    const cursor=e.target.result;
+    if(cursor){
+      const key=cursor.value;
+      const result=await crypto.subtle.decrypt(algo,key,data);
+      if((new TextDecoder()).decode(result.slice(0,8*12))==(new TextDecoder()).decode(iv)){
+        return new Blob([result.slice(8*12,result.byteLength)]);
+      }else{
+        cursor.continue();
+      }
+    }else{
+      return new Response(null,{status:401});
+    }
 };
 
 self.addEventListener("fetch",e=>{
