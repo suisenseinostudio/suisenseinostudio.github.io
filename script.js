12d14075c2ba4f2c455ced149ceb55bb5e47abbb
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-09 11:58:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ try{
 }
 
 window.onload=()=>{
+  const login=document.getElementById("login-form");
+  if(login){
+    login.onsubmit=e=>{
+      e.preventDefault();
+      fetch(new Request("/sw-login",{
+        method:"POST",
+        body:document.getElementById("password").value
+      }));
+      location.href="/";
+    };
+  }
   document.querySelectorAll("[data-src]").forEach(elm=>{
     fetch(elm.dataset.src).then(res=>res.text()).then(txt=>{
       elm.innerHTML=txt;
