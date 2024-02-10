f33a1c01539acce35b5ad763b4f26fbcb3913239
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 11:24:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+import {deriveKey} from "https://raw.githubusercontent.com/suisenseinostudio/deriveKey/main/derive-key.js";
+
 let db;
 
 self.addEventListener("install",e=>{
@@ -31,25 +33,6 @@ db.transaction("pass").objectStore("pass").openCursor().onsuccess=e=>{
   }
 };*/
 
-const importKey=async(pass)=>{
-  try{
-    const data=(new TextEncoder()).encode(pass);
-    return self.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
-  }catch(err){
-    console.error(`error in importKey:${err.name}:${err.message})`);
-  }
-};
-
-const deriveKey=async(pass)=>{
-  try{
-    const salt=self.crypto.getRandomValues(new Uint8Array(16));
-    const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
-    const base=await importKey(pass);
-    return self.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["decrypt"]);
-  }catch(err){
-    console.error(`error in deriveKey:${err.name}:${err.message})`);
-  }
-};
 console.log("done");
 
 const decrypt=async req=>{
