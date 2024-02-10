6ff40cb41d04790d87a669b4804656a204925372
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 11:35:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -48,8 +48,9 @@ const decrypt=async req=>{
       const key=await deriveKey(pass);
       console.log("derived key...");
       try{
+        console.log(`dec(${algo},key(${pass.value}),${data})`);
         const result=await crypto.subtle.decrypt(algo,key,data);
-        console.log("decrypted...");
+        console.log(`result:${result}`);
         if((new TextDecoder()).decode(result.slice(0,12))==(new TextDecoder()).decode(iv)){
           console.log("succeeded!");
           return new Blob([result.slice(12,result.byteLength)]);
