f3836c2824ca505fd7a63ca471742f2e65cce47c
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-13 11:32:47 +0000

    Indent crypt.js

--- js/crypt.js
+++ js/crypt.js
@@ -1,10 +1,29 @@
-try{const a=navigator.serviceWorker.register("/sw.js",{scope:"/",type:"module"})}catch(e){console.error(e)}window.onload=()=>{var e=document.getElementById("login-form");e&&(e.onsubmit=e=>{e.preventDefault(),fetch(new Request("/sw-login",{method:"POST",body:document.getElementById("password").value})),location.href="/"}),document.querySelectorAll("[data-plain]").forEach(l=>{fetch(l.dataset.plain).then(e=>(401==e.status&&(location.href="/login.html"),e.text())).then(e=>{l.innerHTML=e})}),document.querySelectorAll("[data-src]").forEach(l=>{fetch(l.dataset.src).then(e=>(401==e.status&&(location.href="/login.html"),e.text())).then(e=>{
-  l.innerHTML=e
-    .replaceAll(/^([^#].*\S.*)$/gm,"<p>$1</p>")
-    .replaceAll(/^##([^#].*\S.*)$/gm,"<h3>$1</h3>")
-    .replaceAll(/^###([^#].*\S.*)$/gm,"<h4>$1</h4>")
-    .replaceAll(/^####([^#].*\S.*)$/gm,"<h5>$1</h5>")
-    .replaceAll(/\*\*(.*?\S.*?)\*\*/gm,"<span class='marker'>$1</span>")
-    .replaceAll(/\+\[(.*?\S.*?)\]\((.*?\S.*?)\)/gm,"<a href='$2' class='normal-link' target='_blank' rel='noopener noreferrer'>$1</a>")
-    .replaceAll(/\[(.*?\S.*?)\]\((.*?\S.*?)\)/gm,"<a href='$2' class='normal-link'>$1</a>")
+try{
+  const a=navigator.serviceWorker.register("/sw.js",{scope:"/",type:"module"})
+}catch(e){console.error(e)}
+window.onload=()=>{
+  var e=document.getElementById("login-form");
+  e&&(e.onsubmit=e=>{
+    e.preventDefault(),
+    fetch(new Request("/sw-login",{method:"POST",body:document.getElementById("password").value})),
+    location.href="/"
+  }),
+  document.querySelectorAll("[data-plain]").forEach(l=>{
+    fetch(l.dataset.plain)
+      .then(e=>(401==e.status&&(location.href="/login.html"),e.text()))
+      .then(e=>{l.innerHTML=e})
+  }),
+  document.querySelectorAll("[data-src]").forEach(l=>{
+    fetch(l.dataset.src)
+      .then(e=>(401==e.status&&(location.href="/login.html"),e.text()))
+      .then(e=>{
+        console.log(e)
+        l.innerHTML=e
+          .replaceAll(/^([^#].*\S.*)$/gm,"<p>$1</p>")
+          .replaceAll(/^##([^#].*\S.*)$/gm,"<h3>$1</h3>")
+          .replaceAll(/^###([^#].*\S.*)$/gm,"<h4>$1</h4>")
+          .replaceAll(/^####([^#].*\S.*)$/gm,"<h5>$1</h5>")
+          .replaceAll(/\*\*(.*?\S.*?)\*\*/gm,"<span class='marker'>$1</span>")
+          .replaceAll(/\+\[(.*?\S.*?)\]\((.*?\S.*?)\)/gm,"<a href='$2' class='normal-link' target='_blank' rel='noopener noreferrer'>$1</a>")
+          .replaceAll(/\[(.*?\S.*?)\]\((.*?\S.*?)\)/gm,"<a href='$2' class='normal-link'>$1</a>")
 })})};
